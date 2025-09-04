1bebcbdc6a3ce80920139474069d49b9c663bac0
Damien Retzinger <damienwebdev@gmail.com>
2025-09-04 10:04:51 -0400

    build(commerce): use dep versions from root package

--- tools/schematics/ng-add/generators/dependencies.ts
+++ tools/schematics/ng-add/generators/dependencies.ts
@@ -9,14 +9,15 @@ import {
 } from '@schematics/angular/utility/dependencies';
 
 import { NgAddOptions } from '../schema';
+import { version } from './version';
 
 export function addDependenciesToPackageJson(options: NgAddOptions): Rule {
   return (tree: Tree) => {
     const dependencies: NodeDependency[] = [
-      { type: NodeDependencyType.Default, version: '^0.87.0', name: '@daffodil/core' },
-      { type: NodeDependencyType.Default, version: '^0.87.0', name: '@daffodil/driver' },
-      { type: NodeDependencyType.Default, version: '^0.87.0', name: '@daffodil/product' },
-      { type: NodeDependencyType.Default, version: 'file:../../graycore/daffodil/dist/dev-tools/daffodil-dev-tools-0.0.0-PLACEHOLDER.tgz', name: '@daffodil/dev-tools' },
+      { type: NodeDependencyType.Default, version: `^${version}`, name: '@daffodil/core' },
+      { type: NodeDependencyType.Default, version: `^${version}`, name: '@daffodil/driver' },
+      { type: NodeDependencyType.Default, version: `^${version}`, name: '@daffodil/product' },
+      { type: NodeDependencyType.Default, version: `^${version}`, name: '@daffodil/dev-tools' },
     ];
 
     dependencies.forEach(dependency => {
