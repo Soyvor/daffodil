cfec8dfd05c959ef7aab463d7095a951d3277e82
Ammar Ahmad <ammarahmadwrk@gmail.com>
2025-09-15 12:51:44 -0400

    feat(docs-utils): add doc model factories (#3907)
    ---------
    
    Co-authored-by: Peter Lauck <griest024@gmail.com>

--- libs/docs/testing/src/factories/public_api.ts
+++ libs/docs/testing/src/factories/public_api.ts
@@ -1,1 +1,6 @@
-export {};
+export * from './api/public_api';
+export * from './doc/public_api';
+export * from './example/public_api';
+export * from './nav/public_api';
+
+export * from './item.factory';
